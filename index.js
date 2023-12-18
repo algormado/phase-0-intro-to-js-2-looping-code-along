e93d8e20798745cdc630bc7beb9efeeafad8ca25
Joshua <algormado@gmail.com>
2023-12-18 14:55:12 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(arrayName, eventName) {
+    const message = [];
+    for (let i = 0; i < arrayName.length; i++) {
+        message[i] = `Thank you, ${arrayName[i]}, for the wonderful ${eventName} gift!`;
+    } 
+    return message;
+  }
+  function countDown(num) {
+    let i = num;
+  
+    while (i >= 0) {
+      console.log(i);
+      i--;
+    }
+  }
+    
+  
